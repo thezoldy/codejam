c89758125e63c52b91c0c7a56c5e41b8a75ffcd6
thezoldy <leoneluncore@hotmail.com>
2023-02-03 20:20:50 -0300

    Correção de Bugs

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,8 @@
+// for youtube
 const btn = document.querySelector("#searchbotton")
 btn.addEventListener("click", function(event){
 event.preventDefault()
+navigationPreload: false    
 const input = document.querySelector("#searchbox")
 const link = input.value
 const newcode = link.slice(32)
@@ -8,3 +10,5 @@ const newlink = `https://www.youtube.com/embed/${newcode}`
 console.log(newlink)
 document.getElementById("linkador").src = [newlink]
 })
+
+//for sharepoint
